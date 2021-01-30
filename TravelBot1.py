d812ea143760df4b32cf09e704fdbd82d270ab7a
faithrts <68522912+faithrts@users.noreply.github.com>
2021-01-30 09:55:45 -0500

    Delete TravelBot1.py

--- TravelBot1.py
+++
@@ -1,6 +0,0 @@
-import discord
-from discord.ext import commands
-
-client = discord.Client() # establishing connection with discord
-
-bot = commands.Bot(command_prefix='.') # establishing command prefix
