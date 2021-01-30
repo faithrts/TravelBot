4d9cb8c8cf7c9ef3af96ba2d3c8d19350da18e1a
srey-626 <78243116+srey-626@users.noreply.github.com>
2021-01-30 15:41:23 -0500

    choose place, recipe, or music

--- TravelBot.py
+++ TravelBot.py
@@ -11,8 +11,13 @@ async def on_ready():
 
 """ call to the bot; bot prints "Where do you want to go?"
 
-testing testing
-
+# choose place, recipe, music
+@client.command()
+async def prm_choice(ctx):
+	await ctx.send("What would you like from this city?\n\U0001F9ED Place\n\U0001F37D Recipe\n\U0001F3B5 Music")
+	await message.add_reaction("\U0001F9ED")
+	await message.add_reaction("\U0001F37D")
+	await message.add_reaction("\U0001F3B5")
 
 """
 
